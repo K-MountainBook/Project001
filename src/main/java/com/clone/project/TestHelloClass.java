e33f2c795d09bef2801743cddfdb2a2b112c58aa
ky-tis <yamamoto.keita@tis.co.jp>
2020-04-03 15:59:23 +0900

    Twitter関連のライブラリをpomに追加

--- src/main/java/com/clone/project/TestHelloClass.java
+++ src/main/java/com/clone/project/TestHelloClass.java
@@ -1,10 +1,17 @@
 package com.clone.project;
 
+//import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+//import twitter4j.Status;
+//import twitter4j.Twitter;
+//import twitter4j.TwitterException;
+//import twitter4j.TwitterFactory;
 
 @Controller
 public class TestHelloClass {
@@ -12,6 +19,7 @@ public class TestHelloClass {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView index(ModelAndView mv) {
 		mv.setViewName("index");
+
 		return mv;
 	}
 
@@ -19,6 +27,19 @@ public class TestHelloClass {
 	public ModelAndView send(@RequestParam("inputValue") String inputvalue, ModelAndView mv) {
 		mv.setViewName("result");
 		mv.addObject("message", inputvalue);
+//		try {
+//			Twitter twitter = TwitterFactory.getSingleton();
+//			List<Status> statuses = twitter.getHomeTimeline();
+//			System.out.println("drowing home timeline");
+//
+//			for (Status status : statuses) {
+//
+//				System.out.println(status.getUser().getName() + ":" + status.getText());
+//			}
+//
+//		} catch (TwitterException e) {
+//			e.printStackTrace();
+//		}
 		return mv;
 	}
 
