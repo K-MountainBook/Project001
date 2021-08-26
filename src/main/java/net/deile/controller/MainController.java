b970e54364cab7108556ab2d932068f2dec8e63c
KeitaYamamoto <keita.yamamoto@mail.o-hara.ac.jp>
2021-08-26 15:38:13 +0900

    20210826_2

--- src/main/java/net/deile/controller/MainController.java
+++ src/main/java/net/deile/controller/MainController.java
@@ -1,15 +1,16 @@
 package net.deile.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class indexController {
+public class MainController {
 
 	@GetMapping("")
-	public String index() {
+	public String index(Model model) {		
 		return "product/dashboard";
 	}
 	
