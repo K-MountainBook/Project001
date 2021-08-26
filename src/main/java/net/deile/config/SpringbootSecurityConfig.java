9182b34490e9d37928ca12c0dd9c2a00ca95102d
Keita Yamamoto <keita.yamamoto@mail.o-hara.ac.jp>
2021-08-26 17:21:51 +0900

    20210826_5

--- src/main/java/net/deile/config/SpringbootSecurityConfig.java
+++ src/main/java/net/deile/config/SpringbootSecurityConfig.java
@@ -1,6 +1,7 @@
 package net.deile.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,6 +20,11 @@ public class SpringbootSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().permitAll();
 //		http.authorizeRequests().antMatchers("/product/**").permitAll().anyRequest().permitAll();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		super.configure(auth);
 	}
 
 }
