5d680d4f2ae528be07788688c9174fe389cae810
keita yamamoto <yamamoto.keita@nifty.com>
2021-09-05 17:49:23 +0900

    change application name

--- src/main/java/net/deile/SpringBootProjectApplication.java
+++ src/main/java/net/deile/SpringBootProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AtndCloneProjectApplication {
+public class SpringBootProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AtndCloneProjectApplication.class, args);
+		SpringApplication.run(SpringBootProjectApplication.class, args);
 	}
 
 }
